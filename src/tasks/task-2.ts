35adf1b800a8805d1e1df7fad37588e1530143e9
marckraw <marckraw@icloud.com>
2023-11-14 23:51:34 +0100

    fix: env config, start using @mrck-labs/anton-sdk

--- src/tasks/task-2.ts
+++ src/tasks/task-2.ts
@@ -1,16 +1,27 @@
+import "../helpers/env-config.js"
 import {requestTask, requestToken, submitAnswer} from "../helper.js";
 import chalk from "chalk";
-import {moderation} from "../helpers/moderation.js";
+import {Anton} from '@mrck-labs/anton-sdk'
+
+
 
 export const task2 = async () => {
     const { token } = await requestToken('moderation')
+    const anton = new Anton(process.env.OPENAI_KEY)
     if(token) {
         const task = await requestTask(token)
         console.log(chalk.blue("This is task: "))
         console.log(task)
         console.log(" ")
 
-        const result = await moderation({toModerate: task.input})
+        const data = await anton.moderation(task.input)
+
+        console.log("This is result: ")
+        console.log(data.results)
+
+        const result = data.results
+
+        // const result = await moderation({toModerate: task.input})
 
         const resultArray = result.map(item => {
             if(item.flagged) {
